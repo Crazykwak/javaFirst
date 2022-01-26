045a45991ecf71307ae29ccbbd84dd3fc1133c19
Crazykwak <kwj1830@naver.com>
2022-01-26 19:49:13 +0900

    var method create test

--- src/first/Test.java
+++ src/first/Test.java
@@ -12,17 +12,19 @@ public class Test {
         return Arrays.toString(result);
     }
 
-
-
-    public static void main(String[] args){
-        Test met = new Test();
-        String a = "Hello Java";
-        System.out.println(met.arrStr(a));
+    public void var(){
         System.out.println("Byte의 크기 " + Byte.SIZE + "비트" + " -Byte의 범위 " + Byte.MIN_VALUE + " ~ " + Byte.MAX_VALUE);
         System.out.println("short의 크기 " + Short.SIZE + "비트" + " -short의 범위 " + Short.MIN_VALUE + " ~ " + Short.MAX_VALUE);
         System.out.println("int의 크기 " + Integer.SIZE + "비트" + " -int의 범위 " + Integer.MIN_VALUE + " ~ " + Integer.MAX_VALUE);
         System.out.println("long의 크기 " + Long.SIZE + "비트" + " -long의 범위 " + Long.MIN_VALUE + " ~ " + Long.MAX_VALUE);
         System.out.println("char의 크기 " + Character.SIZE + "비트" + " -char의 범위 " + (int)Character.MIN_VALUE + " ~ " + (int)Character.MAX_VALUE);
+    }
+
+    public static void main(String[] args){
+        Test met = new Test();
+        String a = "Hello Java";
+        System.out.println(met.arrStr(a));
+        met.var();
     }
 }
 
