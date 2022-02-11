0efa205e22ced3f81e2eea6dfb8faa6012b3a26a
Crazykwak <kwj1830@naver.com>
2022-02-11 20:49:02 +0900

    interface and abstract class, extends and implements

--- src/first/Test.java
+++ src/first/Test.java
@@ -3,12 +3,11 @@ package first;
 
 public class Test {
     public static void main(String[] args){
-        bird chic = new bird("뺙뺙이");
-        chic.cry();
-        chic.myName();
-        dog choco = new dog("초코");
-        choco.cry();
-        choco.myName();
+        Student k = new Student("한구",17, 77);
+        k.who();
+        k.in(30000, "복권");
+        k.out(15000, "음주가무");
+        k.train(15000, "경마");
 
     }
 }
---
+++ src/first/part4oop2.java
@@ -0,0 +1,47 @@
+package first;
+
+public class part4oop2 {
+}
+
+class Person {
+    String name;
+    int age;
+    int weight;
+
+    Person(){}
+    Person(String name, int age, int weight) {
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+    }
+    void wash(){System.out.println("씻다");}
+    void study(){System.out.println("공부하다");}
+    void play(){System.out.println("놀다");}
+    void who(){System.out.printf("%s는 %d살이며 %d kg이다. \n", name, age, weight);}
+}
+
+interface Allowance{
+    abstract void in(int price, String name);
+    abstract void out(int price, String name);
+}
+
+interface Train{
+    abstract void train(int price, String name);
+}
+
+class Student extends Person implements Allowance, Train{
+
+    Student(String name, int age, int weight){
+        super(name, age, weight);
+    }
+
+    public void in(int price, String name){
+        System.out.println(price + "원 을" + name + "에게 받았습니다.");
+    }
+    public void out(int price, String name){
+        System.out.println(price + "원 을을" + name + "에 썼습니다.");
+    }
+    public void train(int price, String name){
+        System.out.printf("%s(을)를 %d원에 공부했다.", name, price);
+    }
+}
