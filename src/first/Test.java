643d0a4c86b3abf8b4579e208576c210b1406a19
Crazykwak <kwj1830@naver.com>
2022-02-15 13:12:26 +0900

    ladder -1 +1

--- src/first/Test.java
+++ src/first/Test.java
@@ -2,30 +2,19 @@ package first;
 
 
 import java.util.Arrays;
-import java.util.Random;
+import java.util.Collections;
 
 public class Test {
 
     public static void main(String[] args){
 
-        MaxMin a = new MaxMin();
+        Integer[] a = {88, 50, 38, 100, 90, 100, 99, 65};
 
-        int[] randomNum = new int[10];
-        for(int i = 0; i < randomNum.length; i++){
-            randomNum[i] = (int)(Math.random()*100);
-        }
+        Arrays.sort(a);
+        System.out.println("오름차순 정렬 : "+ Arrays.toString(a));
 
-        System.out.println("Math로 생성된 숫자 " + Arrays.toString(randomNum));
-        System.out.println("[최대값, 최소값] = " + Arrays.toString(a.maxmin(randomNum)));
-
-
-        Random lott = new Random();
-        for(int i = 0; i < randomNum.length; i++){
-            randomNum[i] = lott.nextInt(101);
-        }
-
-        System.out.println("Random 클래스로 생성된 숫자 = "+Arrays.toString(randomNum));
-        System.out.println("[최대값, 최소값] = " + Arrays.toString(a.maxmin(randomNum)));
+        Arrays.sort(a, Collections.reverseOrder());
+        System.out.println("내림차순 정렬 : "+ Arrays.toString(a));
     }
 }
 
